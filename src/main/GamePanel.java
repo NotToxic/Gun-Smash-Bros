b54e72673a5d6d264891d2bb6be6ce5d1a8a2db1
thatdudePranay <sspranay20@gmail.com>
2023-12-20 14:24:13 -0500

    Opening and Writing CSV Map File

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,6 +1,11 @@
 package main;
 
 import java.awt.*;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 import javax.swing.*;
 
 import inputs.KeyInputs;
@@ -11,6 +16,7 @@ public class GamePanel extends JPanel{
   Player player = new Player(300, 500, 45, 90, this);
 
   JButton backButton = new JButton("Back");
+  BufferedReader map1CSV;
 
   @Override
   public void paintComponent(Graphics g) {
@@ -36,5 +42,45 @@ public class GamePanel extends JPanel{
     backButton.setSize(200,200);
     backButton.setLocation(100,100);
     add(backButton);
+
+    
+    try{ 
+      map1CSV = new BufferedReader(new FileReader("CPTMap1.csv"));
+      System.out.println("File FOund");
+    }catch(FileNotFoundException e){
+      System.out.println("File Not found");
+    }
+    
+    String[][] strMap = new String[160][90];
+
+    String strLine;
+    String strSplit[];
+    int intCount;
+    int intCounter;
+
+    for(intCount = 0; intCount <= 89; intCount++){
+      try{
+        strLine = map1CSV.readLine();
+        strSplit = strLine.split(",");
+          for(intCounter = 0; intCounter <=159; intCounter++){
+            strMap[intCount][intCounter] = strSplit[intCounter];
+          }
+          map1CSV.close();
+        }catch(IOException e){
+          System.out.println("Error");
+        }
+    }
+
+    int intCount2;
+    int intCount3;
+
+    for(intCount2 = 0; intCount2 <=159; intCount2++){
+      for(intCount3 = 0; intCount3 <= 89; intCount3++){
+        System.out.println(strMap[intCount2][intCount3]);
+      }
+
+    }
+  
   }
 }
+
